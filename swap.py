d02b5cfbae5782169e242ecc14e9e6b0f4eebae9
sokt777 <vebegaruzhenko@gmail.com>
2026-09-20 14:19:53 +0300

    Update swap.py

---
+++ swap.py
@@ -0,0 +1,14 @@
+first_room = "401"
+second_room = "227"
+
+print("До обмена")
+print("Aуд1", first_room)
+print("Ауд2", second_room)
+
+temp = first_room
+first_room = second_room
+second_room = temp
+
+print("После обмена")
+print("Aуд1", first_room)
+print("Ауд2", second_room)

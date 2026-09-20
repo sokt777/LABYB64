59d90f105765efcfabfdcc661247fa2ed4cc79d9
sokt777 <vebegaruzhenko@gmail.com>
2026-09-20 12:46:28 +0300

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,10 @@
+Familia = ('Маруженко')
+Name = ('Иван')
+Group = ('Группа УБ - 64')
+City = ('Воронеж')
+Age = ('18 лет')
+lub_pr =  ('Люимый предмет математика')
+hour_in_weak = ('4 часа подготовки в неделю')
+print(Familia,Name,Group,City,Age,lub_pr,hour_in_weak)
+
+

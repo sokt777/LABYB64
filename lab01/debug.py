fa202a729a3d4b1e62f8a8c9d291e0c0e8fe531c
sokt777 <vebegaruzhenko@gmail.com>
2026-09-26 12:32:55 +0300

    Add files via upload

---
+++ lab01/debug.py
@@ -0,0 +1,17 @@
+print(" Фрагмент А ")
+first = int("2")
+second = int("3")
+print(first + second)
+#Нельзя прибовлять струки, для решения нужно напистаь int для того что бы python считал не как строку а как число
+
+print(" Фрагмент Б ")
+age = int(input("Возраст: "))
+print(age + 1)
+
+print(" Фрагмент В ")
+first = 4
+second = 7
+third = 10
+average = (first + second + third) / 3
+print(average)
+#Добавили скобки что бы сначоло было прибаление а только после деление по правилам математики

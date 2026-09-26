c927dbcdf73808455b58485fd4c444926322e513
sokt777 <vebegaruzhenko@gmail.com>
2026-09-26 12:42:09 +0300

    Delete variantN2.py

--- variantN2.py
+++
@@ -1,9 +0,0 @@
-obsh_obem = int(input("Введите Количество саженцев: "))
-vmest_1sazh = int(input("Введите вместимость одной единицы: "))
-total = obsh_obem // vmest_1sazh
-ostat = obsh_obem % vmest_1sazh
-min = (obsh_obem + vmest_1sazh - 1) // vmest_1sazh
-print("Количество заполненных рядов:", total)
-print("Остаток саженцев:", ostat)
-print("Минимальное число рядов:", min)
-
